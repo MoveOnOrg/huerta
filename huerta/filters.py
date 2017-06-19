0e5c01d68e6da841afcfb69439fa5fca1b9077b4
Sandra Chung <sandra@moveon.org>
2017-06-19 12:24:17 -0600

    beginnings of new single select collapsible list filter

--- huerta/filters.py
+++ huerta/filters.py
@@ -170,3 +170,72 @@ class CollapsedListFilter(AllValuesFieldListFilter):
                 'multiselect': True,
                 'value': 'EMPTY'
             }
+
+class CollapsedListFilterSingle(SimpleListFilter):
+    template = 'admin/collapsed_filter_single.html'
+    """
+        Collapsed List Filter for single select
+    """     
+    def selected_choices(self, cl):
+        self.parameter_name = self.lookup_kwarg
+        if self.lookup_val is None and self.lookup_val_isnull is None:
+            yield {
+                'parameter_name': self.parameter_name,
+                'selected': (self.lookup_val is None
+                    and self.lookup_val_isnull is None),
+                'query_string': cl.get_query_string({},
+                    [self.lookup_kwarg, self.lookup_kwarg_isnull]),
+                'display': _('All'),
+                'value': '',
+            }
+        include_none = False
+        for val in self.lookup_choices:
+            pk_val = str(val)
+            display_val = str(val)
+            if isinstance(val, tuple):
+                #remote_field case
+                pk_val = str(val[0])
+                display_val = val[1]
+            if val is None:
+                include_none = True
+                continue
+            display_val = smart_text(display_val)
+            val_choice_list = [] if self.lookup_val is None else self.lookup_val.split(',')
+            selected = (pk_val in val_choice_list) #DIFF
+            remove = [self.lookup_kwarg_isnull]
+            change = {}
+            # ugly matrix of when to remove the val when selected
+            # vs when to append it to the existing? list
+            if val_choice_list:
+                if selected:
+                    # choice should be to remove it
+                    val_choice_list.remove(pk_val)
+                    if len(val_choice_list) == 0:
+                        remove.append(self.lookup_kwarg)
+                    else:
+                        change[self.lookup_kwarg] = ','.join(val_choice_list)
+                else:
+                    val_choice_list.append(pk_val)
+                    change[self.lookup_kwarg] = ','.join(val_choice_list)
+            else:
+                change[self.lookup_kwarg] = pk_val
+            if selected:
+                yield {
+                    'parameter_name': self.parameter_name,
+                    'selected': selected,
+                    'query_string': cl.get_query_string(change, remove),
+                    'display': display_val,
+                    'multiselect': True,
+                    'value': pk_val,
+                }
+        if include_none and bool(self.lookup_val_isnull):
+            yield {
+                'parameter_name': self.parameter_name,
+                'selected': bool(self.lookup_val_isnull),
+                'query_string': cl.get_query_string({
+                    self.lookup_kwarg_isnull: 'True',
+                }, [self.lookup_kwarg]),
+                'display': self.empty_value_display,
+                'multiselect': True,
+                'value': 'EMPTY'
+            }
